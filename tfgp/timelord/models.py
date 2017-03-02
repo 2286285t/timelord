52801a76d10ecb50af3af37b5fc872a3e5596b82
CHS00047 <2288540S@student.gla.ac.uk>
2017-03-02 09:54:40 +0000

    Category model added

--- tfgp/timelord/models.py
+++ tfgp/timelord/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.template.defaultfilters import slugify
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -16,4 +17,25 @@ class UserAccount(models.Model):
     def __unicode__(self):
         return self.user.username
 
-    
+
+class Category(models.Model):
+    max_l1 = 50
+    name = models.CharField(max_length=max_l1, unique=True)
+    description = models.CharField(max_length=250)
+    colour = models.CharField(max_length = 20)
+    ownerUsername = models.CharField(max_length = max_l1)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.description
+
+    def __unicode__(self):
+        return self.description
