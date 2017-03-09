9b2c8dcd31c70540d484de85747964026974305b
Helene Waechtler <2055747W@student.gla.ac.uk>
2017-03-09 10:13:48 +0000

    Fix name of account view in urlpatterns

--- tfgp/timelord/urls.py
+++ tfgp/timelord/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^createtask/$', views.create_task, name='create_task'),
     url(r'^timetable/edittask/$', views.edit_task, name='edit_task'),
     url(r'^timetable/editcategories/$', views.edit_categories, name='edit_categories'),
-    url(r'^account/$', views.account, name='account'),
+    url(r'^account/$', views.view_account, name='account'),
     url(r'^logout/$', views.user_logout, name='logout'),
 ]
