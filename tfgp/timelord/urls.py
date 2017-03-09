f59353480378ac2156f5d5658a806ffd9a6bf2eb
Helene Waechtler <2055747W@student.gla.ac.uk>
2017-03-09 09:17:27 +0000

    Add urlpatterns

--- tfgp/timelord/urls.py
+++ tfgp/timelord/urls.py
@@ -3,9 +3,13 @@ from timelord import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.user_login, name='login'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^timetable/$', views.timetable, name='timetable'),
-    
+    url(r'^createtask/$', views.create_task, name='create_task'),
+    url(r'^timetable/edittask/$', views.edit_task, name='edit_task'),
+    url(r'^timetable/editcategories/$', views.edit_categories, name='edit_categories'),
+    url(r'^account/$', views.account, name='account'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
